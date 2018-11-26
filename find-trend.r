c4f074c5d78d204af33b61b794c77cd08c7e7c06
Muhammad Ali Farooqi <m.ali_farooqi@hotmail.com>
2018-11-26 17:45:46 +0800

    Added category success by year

--- find-trend.r
+++ find-trend.r
@@ -3,7 +3,7 @@ library(ggplot2)
 library(pROC)
 
 # To be modified if using mac
-ksdf = read.csv("C:\\Users\\ali1997\\Downloads\\PredictingKickstarters\\ks-projects-201801.csv", header = TRUE)
+ksdf = read.csv("F:\\R\\PredictingKickstarters\\ks-projects-201801.csv", header = TRUE)
 
 # Converting the launch and deadline Dates to correct format:
 ksdf$launch_date <- as.Date(ksdf$launched, "%Y-%m-%d")
@@ -28,14 +28,16 @@ ggplot(subset(ksdf,launch_year %in% c("2016")), aes(x=main_category)) +
   facet_wrap(~launch_year)
 
 # Top 3 categories in a year (2016) [Used in for loop later]
-names(head(sort(table(subset(ksdf,launch_year %in% c("2016"))$main_category), decreasing = T), 3))
+names(sort(table(subset(ksdf,launch_year %in% c("2016"))$main_category), decreasing = T))
+subset(ksdf,state == "successful")[, 1:10]
 
 # Top 3 categories in each year
 all_years = sort(unique(ksdf$launch_year))
 trend = data.frame(matrix(NA, nrow = 3, ncol = length(all_years)))
 idx = 1
 for (year in all_years){
-  topThree = names(head(sort(table(subset(ksdf,launch_year %in% c(year))$main_category), decreasing = T), 3))
+  dataOfTheYear = subset(ksdf,launch_year %in% c(year))
+  topThree = names(head(sort(table(dataOfTheYear$main_category), decreasing = T), 3))
   trend[, idx] = topThree
   names(trend)[idx] <- year
   idx = idx+1
@@ -43,3 +45,44 @@ for (year in all_years){
 
 #Display trends
 trend
+
+#write trend data to file 
+write.csv(trend, file = "F:\\R\\PredictingKickstarters\\trends-by-year.csv")
+
+
+# Trends by country
+all_countries = sort(unique(ksdf$country))
+trend = data.frame(matrix(NA, nrow = 3, ncol = length(all_countries)))
+idx = 1
+for (cntry in all_countries){
+  topThree = names(head(sort(table(subset(ksdf,country %in% c(cntry))$main_category), decreasing = T), 3))
+  trend[, idx] = topThree
+  names(trend)[idx] <- cntry
+  idx = idx+1
+}
+
+#write trend data to file 
+write.csv(trend, file = "F:\\R\\PredictingKickstarters\\trends-by-country.csv")
+
+
+# Categories success % in each year
+all_years = sort(unique(ksdf$launch_year))[2:10]
+all_categories = sort(unique(ksdf$main_category))
+trend = data.frame(matrix(NA, nrow = length(all_categories), ncol = length(all_years)))
+idx = 1
+for (year in all_years){
+  dataOfTheYear = subset(ksdf,launch_year %in% c(year))
+  totalRecordsPerCategories = table(dataOfTheYear$main_category)
+  
+  successfulDataOfTheYear = subset(dataOfTheYear,state=="successful")
+  successfulRecordsPerCategories = table(successfulDataOfTheYear$main_category)
+  
+  trend[, idx] = successfulRecordsPerCategories/totalRecordsPerCategories*100
+  names(trend)[idx] <- year
+  idx = idx+1
+}
+row.names(trend) = all_categories
+trend
+
+#write trend data to file 
+write.csv(trend, file = "F:\\R\\PredictingKickstarters\\percentage-success-by-year.csv")
