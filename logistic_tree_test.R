d8b65f7725247bf56fac1c38d8f5b973c1de6cf5
Kandan <kandancd@gmail.com>
2018-11-26 19:03:32 +0800

    Modified Logisitic and tree test

--- logistic_tree_test.R
+++ logistic_tree_test.R
@@ -1,8 +1,8 @@
-data = read.csv("ks-projects-201801.csv")
+data = read.csv("cleaned-ks-data.csv")
 data = data[data$state == 'successful' | data$state == 'failed', ]
 data$state = factor(data$state)
 train = sample(nrow(data), 10000)
-glm.fits=glm(state???usd_goal_real+category, data=data , family = binomial, subset = train)
+glm.fits=glm(state~usd_goal_real+category+duration, data=data , family = binomial, subset = train)
 test_data = data[-train,]
 glm.fits$xlevels[["category"]] <- union(glm.fits$xlevels[["category"]], levels(test_data$category))
 glm.probs = predict(glm.fits,test_data, type='response')
@@ -10,6 +10,6 @@ glm.pred = rep("Failed", nrow(test_data))
 glm.pred[glm.probs>0.5] = 'successful'
 table(glm.pred, test_data[,'state'])
 library(tree)
-tree.state = tree(state~main_category+usd_goal_real , data )
+tree.state = tree(state~main_category+usd_goal_real+duration , data )
 plot(tree.state)
 text(tree.state, pretty=0)
